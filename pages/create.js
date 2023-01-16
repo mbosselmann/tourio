e8ea1c826eb99960c173f015e674882834f20bfe
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-16 23:25:17 +0100

    Update create page

--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,15 @@
 import Link from "next/link.js";
 import { useRouter } from "next/router";
+import styled from "styled-components";
 import Form from "../components/Form.js";
+import { StyledLink } from "../components/StyledLink.js";
+
+const Wrapper = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
 
 export default function CreatePlacePage() {
   const router = useRouter();
@@ -22,9 +31,12 @@ export default function CreatePlacePage() {
   }
 
   return (
-    <>
-      <Link href="/">back</Link>
-      <Form onSubmit={handleSubmit} />
-    </>
+    <Wrapper>
+      <h2 id="add-place">Add Place</h2>
+      <Link href="/" passHref legacyBehavior>
+        <StyledLink variant="alignSelf">back</StyledLink>
+      </Link>
+      <Form onSubmit={handleSubmit} formName={"add-place"} />
+    </Wrapper>
   );
 }
