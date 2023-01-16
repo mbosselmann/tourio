f4dd4f67ce1a593be52be087560d1e3535fd6b67
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-17 00:31:10 +0100

    Add swr config

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,21 @@ import Layout from "../components/Layout.js";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Layout>
+    <SWRConfig
+      value={{
+        fetcher: async (...args) => {
+          const response = await fetch(...args);
+          if (!response.ok) {
+            throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+          }
+          return await response.json();
+        },
+      }}
+    >
+      <Layout>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
   );
 }
