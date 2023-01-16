9722a39c6156ad6e64250fec177b09782c514342
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-16 15:18:52 +0100

    Update api endpoint to handle POST requests

--- helpers/db.js
+++ helpers/db.js
@@ -1,12 +1,15 @@
+import { v4 as uuidv4 } from "uuid";
 import mongoose, { model, models, Schema } from "mongoose";
 
 const URI = `mongodb+srv://mareikebosselmann:${process.env.MONGODB_PASSWORD}@cluster0.rdlhnaa.mongodb.net/?retryWrites=true&w=majority`;
 
 const placeSchema = new Schema({
   id: String,
-  text: String,
-  author: String,
-  categories: [String],
+  location: String,
+  image: String,
+  mapURL: String,
+  description: String,
+  name: String,
 });
 
 const Place = models.Place || model("Place", placeSchema);
@@ -23,4 +26,14 @@ async function getAllPlaces() {
   return places;
 }
 
-export { getAllPlaces };
+async function createPlace(place) {
+  await connectDatabase();
+
+  const createdPlace = await Place.create({
+    ...place,
+    id: uuidv4(),
+  });
+  return createdPlace;
+}
+
+export { getAllPlaces, createPlace };
