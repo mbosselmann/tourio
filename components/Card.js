aa47c18c0a31e8856f61252dcdb77f91f7f74af9
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-16 12:49:30 +0100

    Add stylings for Card

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,20 @@
 import Image from "next/image.js";
+import styled from "styled-components";
+
+const Article = styled.article`
+  border: 5px solid black;
+  border-radius: 0.8rem;
+  padding: 0.5rem;
+`;
 
 export default function Card({ name, image, location }) {
   return (
-    <article>
+    <Article>
       <figure>
         <Image src={image} width={250} height={150} alt="" />
         <figcaption>{name}</figcaption>
       </figure>
       <p>Location: {location}</p>
-    </article>
+    </Article>
   );
 }
