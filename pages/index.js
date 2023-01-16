b4c9b4ca2468bd527e88ab255a04caef4efd26e3
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-16 12:49:55 +0100

    Create home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,43 @@
+import styled from "styled-components";
+import Card from "../components/Card.js";
+import TitleBar from "../components/TitleBar.js";
+import useSWR from "swr";
+
+const fetcher = async (...args) => {
+  const response = await fetch(...args);
+  if (!response.ok) {
+    throw new Error(`Request with ${JSON.stringify(args)} failed.`);
+  }
+  return await response.json();
+};
+
+const List = styled.ul`
+  list-style: none;
+  margin-top: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  const { data: places } = useSWR("/api/places", fetcher, { fallbackData: [] });
+
+  return (
+    <>
+      <TitleBar />
+      <List>
+        {places.map((place) => {
+          return (
+            <Card
+              key={place.id}
+              name={place.name}
+              image={place.image}
+              location={place.location}
+            />
+          );
+        })}
+      </List>
+    </>
+  );
 }
