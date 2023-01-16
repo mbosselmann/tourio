be43de24f9d39fadfd8451283accf2b003f0e89c
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-17 00:29:49 +0100

    Update card component

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,4 @@
+import Link from "next/link.js";
 import Image from "next/image.js";
 import styled from "styled-components";
 
@@ -7,7 +8,31 @@ const Article = styled.article`
   padding: 0.5rem;
 `;
 
-export default function Card({ name, image, location }) {
+const Anchor = styled.a`
+  &::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+`;
+
+const ScreenReaderOnly = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+`;
+
+export default function Card({ name, image, location, id }) {
   return (
     <Article>
       <figure>
@@ -15,6 +40,11 @@ export default function Card({ name, image, location }) {
         <figcaption>{name}</figcaption>
       </figure>
       <p>Location: {location}</p>
+      <Link href={`places/${id}`} passHref legacyBehavior>
+        <Anchor>
+          <ScreenReaderOnly>More Info</ScreenReaderOnly>
+        </Anchor>
+      </Link>
     </Article>
   );
 }
