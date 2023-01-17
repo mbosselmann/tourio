d13c2331e107de1e27cedf427306a3359995eb2f
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-17 14:14:11 +0100

    Add head and lang

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import TitleBar from "./TitleBar.js";
 import styled from "styled-components";
+import Head from "next/head.js";
 
 const Main = styled.main`
   display: grid;
@@ -7,11 +8,15 @@ const Main = styled.main`
   margin-top: 5rem;
   padding: 0.5rem;
   position: relative;
+  width: 100%;
 `;
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Tourio</title>
+      </Head>
       <TitleBar />
       <Main>{children}</Main>
     </>
