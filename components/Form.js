00c66813b415489151fae159b50e2b4893ea09d5
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-17 11:12:40 +0100

    Update form component

--- components/Form.js
+++ components/Form.js
@@ -34,26 +34,56 @@ const Label = styled.label`
   font-weight: bold;
 `;
 
-export default function Form({ onSubmit, formName }) {
+export default function Form({ onSubmit, formName, defaultData }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmit(data);
+  }
+
   return (
     <>
-      <FormContainer aria-labelledby={formName} onSubmit={onSubmit}>
+      <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
         <Label htmlFor="name">Name</Label>
-        <Input id="name" name="name" type="text" />
+        <Input
+          id="name"
+          name="name"
+          type="text"
+          defaultValue={defaultData?.name}
+        />
         <Label htmlFor="image-url">Image Url</Label>
-        <Input id="image-url" name="image" type="text" />
+        <Input
+          id="image-url"
+          name="image"
+          type="text"
+          defaultValue={defaultData?.image}
+        />
         <Label htmlFor="location">Location</Label>
-        <Input id="location" name="location" type="text" />
+        <Input
+          id="location"
+          name="location"
+          type="text"
+          defaultValue={defaultData?.location}
+        />
         <Label htmlFor="map-url">Map Url</Label>
-        <Input id="map-url" name="mapURL" type="text" />
+        <Input
+          id="map-url"
+          name="mapURL"
+          type="text"
+          defaultValue={defaultData?.mapURL}
+        />
         <Label htmlFor="description">Description</Label>
         <Textarea
           name="description"
           id="description"
           cols="30"
           rows="10"
+          defaultValue={defaultData?.description}
         ></Textarea>
-        <Button type="submit">Add place</Button>
+        <Button type="submit">
+          {defaultData ? "Update place" : "Add place"}
+        </Button>
       </FormContainer>
     </>
   );
