68be9c1a5466e66156a2f1801b3514ce149d29b4
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-17 11:13:46 +0100

    Create edit page

---
+++ pages/places/[id]/edit.js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import useSWR from "swr";
+import { StyledLink } from "../../../components/StyledLink.js";
+import Form from "../../../components/Form.js";
+
+export default function EditPage() {
+  const router = useRouter();
+  const { isReady } = router;
+  const { id } = router.query;
+  const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
+
+  async function editPlace(place) {
+    await fetch(`/api/places/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(place),
+    });
+
+    router.push("/");
+  }
+
+  if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+
+  return (
+    <>
+      <h2 id="edit-place">Edit Place</h2>
+      <Link href="/" passHref legacyBehavior>
+        <StyledLink variant="alignSelf">back</StyledLink>
+      </Link>
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
+    </>
+  );
+}
