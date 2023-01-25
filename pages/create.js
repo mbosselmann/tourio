a9ea6277e1715d5a8da8a004779dcf76aab8e26a
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-25 08:41:09 +0100

    Fix missing import of styled components

--- pages/create.js
+++ pages/create.js
@@ -1,4 +1,5 @@
 import Link from "next/link.js";
+import styled from "styled-components";
 import { useRouter } from "next/router";
 import Form from "../components/Form.js";
 import { StyledLink } from "../components/StyledLink.js";
