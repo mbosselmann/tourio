a49752147e79af80196b078042b9cc5e4de1588f
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2023-01-25 08:38:42 +0100

    Remove fragments

--- components/Form.js
+++ components/Form.js
@@ -33,48 +33,46 @@ export default function Form({ onSubmit, formName, defaultData }) {
   }
 
   return (
-    <>
-      <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
-        <Label htmlFor="name">Name</Label>
-        <Input
-          id="name"
-          name="name"
-          type="text"
-          defaultValue={defaultData?.name}
-        />
-        <Label htmlFor="image-url">Image Url</Label>
-        <Input
-          id="image-url"
-          name="image"
-          type="text"
-          defaultValue={defaultData?.image}
-        />
-        <Label htmlFor="location">Location</Label>
-        <Input
-          id="location"
-          name="location"
-          type="text"
-          defaultValue={defaultData?.location}
-        />
-        <Label htmlFor="map-url">Map Url</Label>
-        <Input
-          id="map-url"
-          name="mapURL"
-          type="text"
-          defaultValue={defaultData?.mapURL}
-        />
-        <Label htmlFor="description">Description</Label>
-        <Textarea
-          name="description"
-          id="description"
-          cols="30"
-          rows="10"
-          defaultValue={defaultData?.description}
-        ></Textarea>
-        <StyledButton type="submit">
-          {defaultData ? "Update place" : "Add place"}
-        </StyledButton>
-      </FormContainer>
-    </>
+    <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
+      <Label htmlFor="name">Name</Label>
+      <Input
+        id="name"
+        name="name"
+        type="text"
+        defaultValue={defaultData?.name}
+      />
+      <Label htmlFor="image-url">Image Url</Label>
+      <Input
+        id="image-url"
+        name="image"
+        type="text"
+        defaultValue={defaultData?.image}
+      />
+      <Label htmlFor="location">Location</Label>
+      <Input
+        id="location"
+        name="location"
+        type="text"
+        defaultValue={defaultData?.location}
+      />
+      <Label htmlFor="map-url">Map Url</Label>
+      <Input
+        id="map-url"
+        name="mapURL"
+        type="text"
+        defaultValue={defaultData?.mapURL}
+      />
+      <Label htmlFor="description">Description</Label>
+      <Textarea
+        name="description"
+        id="description"
+        cols="30"
+        rows="10"
+        defaultValue={defaultData?.description}
+      ></Textarea>
+      <StyledButton type="submit">
+        {defaultData ? "Update place" : "Add place"}
+      </StyledButton>
+    </FormContainer>
   );
 }
